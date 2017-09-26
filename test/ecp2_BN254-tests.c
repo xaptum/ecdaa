6a1980dcee9f909991cce6b3667e0e3c2c429c93
Zane Beckwith <zanebeckwith@aol.com>
2017-09-26 16:25:38 -0500

    Test serialization of ecp and ecp2 points.

--- test/ecp2_BN254-tests.c
+++ test/ecp2_BN254-tests.c
@@ -21,20 +21,28 @@
 #include "../src/amcl-extensions/ecp2_BN254.h"
 
 #include <stdio.h>
+#include <string.h>
 
 static void g2_basepoint_not_inf();
-
+static void g2_serialize_then_deserialize_basepoint();
+static void g2_lengths_same();
+static void g2_deserialize_badformat_fails();
+static void g2_deserialize_badcoords_fails();
 
 int main()
 {
     g2_basepoint_not_inf();
+    g2_serialize_then_deserialize_basepoint();
+    g2_lengths_same();
+    g2_deserialize_badformat_fails();
+    g2_deserialize_badcoords_fails();
 
     return 0;
 }
 
 void g2_basepoint_not_inf()
 {
-    printf("Starting pairing_curve_utils::g2_basepoint_not_inf...\n");
+    printf("Starting ecp2_BN254::g2_basepoint_not_inf...\n");
 
     ECP2_BN254 point;
     ecp2_BN254_set_to_generator(&point);
@@ -44,3 +52,57 @@ void g2_basepoint_not_inf()
     printf("\tsuccess\n");
 }
 
+static void g2_serialize_then_deserialize_basepoint()
+{
+    printf("Starting ecp2_BN254::g2_basepoint_not_inf...\n");
+
+    ECP2_BN254 point;
+    ecp2_BN254_set_to_generator(&point);
+
+    uint8_t buffer[ECP2_BN254_LENGTH];
+
+    ecp2_BN254_serialize(buffer, &point);
+
+    ECP2_BN254 deserialized_point;
+    TEST_ASSERT(0 == ecp2_BN254_deserialize(&deserialized_point, buffer));
+
+    TEST_ASSERT(ECP2_BN254_equals(&point, &deserialized_point));
+
+    printf("\tsuccess\n");
+}
+
+static void g2_lengths_same()
+{
+    printf("Starting ecp2_BN254::g2_lengths_same...\n");
+
+    TEST_ASSERT(ECP2_BN254_LENGTH == ecp2_BN254_length());
+
+    printf("\tsuccess\n");
+}
+
+static void g2_deserialize_badformat_fails()
+{
+    printf("Starting ecp2_BN254::g2_deserialize_badformat_fails...\n");
+
+    uint8_t buffer[ECP2_BN254_LENGTH] = {0};
+    buffer[0] = 0x3;
+
+    ECP2_BN254 point;
+    TEST_ASSERT(-2 == ecp2_BN254_deserialize(&point, buffer));
+
+    printf("\tsuccess\n");
+}
+
+static void g2_deserialize_badcoords_fails()
+{
+    printf("Starting ecp2_BN254::g2_deserialize_badcoords_fails...\n");
+
+    uint8_t buffer[ECP2_BN254_LENGTH] = {0};
+    memset(buffer, 1, sizeof(buffer));  // All 1's
+    buffer[0] = 0x4;
+
+    ECP2_BN254 point;
+    TEST_ASSERT(-1 == ecp2_BN254_deserialize(&point, buffer));
+
+    printf("\tsuccess\n");
+}
