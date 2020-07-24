65f460b3d963ca0c8d2cf3afe3c88cb21ec69200
Zane Beckwith <zanebeckwith@gmail.com>
2020-07-24 19:38:07 +0000

    libecdaa-tpm: Update TSS calls to use tpm2-tss (not xaptum-tpm).

--- test/tpm/tpm_ZZZ-test-utils.h
+++ test/tpm/tpm_ZZZ-test-utils.h
@@ -21,7 +21,7 @@
 #include <ecdaa-tpm/tpm_context.h>
 
 #include <tss2/tss2_sys.h>
-#include <tss2/tss2_tcti_socket.h>
+#include <tss2/tss2_tcti_mssim.h>
 #include <tss2/tss2_tcti_device.h>
 
 #include <string.h>
@@ -31,7 +31,7 @@ const char *handle_filename = "handle.txt";
 
 static
 int read_public_key_from_files(uint8_t *public_key,
-                               TPM_HANDLE *key_handle,
+                               TPM2_HANDLE *key_handle,
                                const char *pub_key_filename,
                                const char *handle_filename);
 
@@ -46,13 +46,12 @@ struct tpm_test_context {
 static
 int tpm_initialize(struct tpm_test_context *ctx)
 {
-    const char *hostname = "localhost";
-    const char *port = "2321";
-    const char *dev_filepath = "/dev/tpm0";
+    const char *mssim_conf = "host=localhost,port=2321";
+    const char *device_conf = "/dev/tpm0";
 
     int ret = 0;
 
-    TPM_HANDLE key_handle = 0;
+    TPM2_HANDLE key_handle = 0;
 
     if (0 != read_public_key_from_files(ctx->serialized_public_key, &key_handle, pub_key_filename, handle_filename)) {
         printf("Error: error reading in public key files '%s' and '%s'\n", pub_key_filename, handle_filename);
@@ -66,24 +65,35 @@ int tpm_initialize(struct tpm_test_context *ctx)
 
     ctx->tcti_context = (TSS2_TCTI_CONTEXT*)ctx->tcti_buffer;
 #ifdef USE_TCP_TPM
-    (void)dev_filepath;
-    if (tss2_tcti_getsize_socket() > sizeof(ctx->tcti_buffer)) {
+    (void)device_conf;
+    size_t size;
+    ret = Tss2_Tcti_Mssim_Init(NULL, &size, mssim_conf);
+    if (TSS2_RC_SUCCESS != ret) {
+        printf("Failed to get allocation size for tcti context\n");
+        return -1;
+    }
+    if (size > sizeof(ctx->tcti_buffer)) {
         printf("Error: socket TCTI context size larger than pre-allocated buffer\n");
         return -1;
     }
-    ret = tss2_tcti_init_socket(hostname, port, ctx->tcti_context);
+    ret = Tss2_Tcti_Mssim_Init(ctx->tcti_context, &size, mssim_conf);
     if (TSS2_RC_SUCCESS != ret) {
         printf("Error: Unable to initialize socket TCTI context\n");
         return -1;
     }
 #else
-    (void)hostname;
-    (void)port;
-    if (tss2_tcti_getsize_device() > sizeof(ctx->tcti_buffer)) {
+    (void)mssim_conf;
+    size_t size;
+    ret = Tss2_Tcti_Device_Init(NULL, &size, device_conf);
+    if (TSS2_RC_SUCCESS != ret) {
+        printf("Failed to get allocation size for tcti context\n");
+        return -1;
+    }
+    if (size > sizeof(ctx->tcti_buffer)) {
         printf("Error: device TCTI context size larger than pre-allocated buffer\n");
         return -1;
     }
-    ret = tss2_tcti_init_device(dev_filepath, strlen(dev_filepath), ctx->tcti_context);
+    ret = Tss2_Tcti_Device_Init(ctx->tcti_context, &size, device_conf);
     if (TSS2_RC_SUCCESS != ret) {
         printf("Error: Unable to initialize device TCTI context\n");
         return -1;
@@ -105,12 +115,12 @@ void tpm_cleanup(struct tpm_test_context *ctx)
     ecdaa_tpm_context_free(&ctx->tpm_ctx);
 
     if (NULL != ctx->tcti_context) {
-        tss2_tcti_finalize(ctx->tcti_context);
+        Tss2_Tcti_Finalize(ctx->tcti_context);
     }
 }
 
 int read_public_key_from_files(uint8_t *public_key,
-                               TPM_HANDLE *key_handle,
+                               TPM2_HANDLE *key_handle,
                                const char *pub_key_filename,
                                const char *handle_filename)
 {
@@ -137,7 +147,7 @@ int read_public_key_from_files(uint8_t *public_key,
     if (NULL == handle_file_ptr)
         return -1;
     do {
-        for (int i=(sizeof(TPM_HANDLE)-1); i >= 0; i--) {
+        for (int i=(sizeof(TPM2_HANDLE)-1); i >= 0; i--) {
             unsigned byt;
             if (fscanf(handle_file_ptr, "%02X", &byt) != 1) {
                 ret = -1;
